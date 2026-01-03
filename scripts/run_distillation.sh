e7fabd208107e424e42757cbc0627cb402dfcefc
EC2 Default User <ec2-user@ip-172-31-21-11.us-east-2.compute.internal>
2026-01-03 03:53:13 +0000

    feat: Add on-policy distillation support

---
+++ scripts/run_distillation.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+. .env
+
+while getopts ":m:n:d:s:o:i:t:b:" opt; do
+  case ${opt} in
+    st ) STUDENT_MODEL=$OPTARG;;
+    te ) TEACHER_MODEL=$OPTARG;;
+    n ) N_ROLLOUTS=$OPTARG;;
+    d ) DATA_PATH=$OPTARG;;
+    s ) CKPT_PATH=$OPTARG;;
+    o ) OTHER_OPTION=$OPTARG;;
+    i ) NUM_INFERENCE_ENGINES=$OPTARG;;
+    t ) NUM_TRAINING_ENGINES=$OPTARG;;
+    b ) MICRO_BATCH_SIZE=$OPTARG;;
+    # \? ) echo "Usage: cmd [-u] [-p]";;
+  esac
+done
+
+STUDENT_MODEL_ALIAS=$(echo $STUDENT_MODEL | sed 's/\//-/g')
+TEACHER_MODEL_ALIAS=$(echo $TEACHER_MODEL | sed 's/\//-/g')
+# Get number of GPUs available
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+N_ROLLOUTS="${N_ROLLOUTS:-8}"
+BATCH_SIZE=32
+MAX_LENGTH=8192
+RUN_NAME="code_search_distillation_${STUDENT_MODEL_ALIAS}_${TEACHER_MODEL_ALIAS}"
+set -x
+
+DATA_PATH="${DATA_PATH:-data/swe_smith}"
+CKPT_PATH="${CKPT_PATH:-$(pwd)/ckpts/${STUDENT_MODEL_ALIAS}}"
+mkdir -p $CKPT_PATH
+
+HALF_NUM_GPUS=$((NUM_GPUS / 2))
+NUM_INFERENCE_ENGINES="${NUM_INFERENCE_ENGINES:-$NUM_GPUS}"
+NUM_TRAINING_ENGINES="${NUM_TRAINING_ENGINES:-$NUM_GPUS}"
+
+export VLLM_FLASH_ATTN_VERSION=2
+export CUDA_LAUNCH_BLOCKING=1
+export TORCH_USE_CUDA_DSA=1
+
+
+uv run --isolated -m src.train \
+  +run_async_trainer=true \
+  +use_distillation=true \
+  data.train_data="['$DATA_PATH/train.parquet']" \
+  data.val_data="['$DATA_PATH/validation.parquet']" \
+  trainer.algorithm.advantage_estimator="no_op" \
+  trainer.algorithm.policy_loss_type="importance_sampling" \
+  trainer.algorith.use_kl_in_reward=true \
+  trainer.algorithm.use_kl_loss=false \
+  trainer.policy.model.path=${STUDENT_MODEL} \
+  trainer.ref.model.path=${TEACHER_MODEL} \
+  trainer.placement.colocate_all=true \
+  trainer.placement.colocate_policy_ref=true \
+  trainer.strategy=fsdp2 \
+  trainer.policy.fsdp_config.cpu_offload=true \
+  trainer.policy.fsdp_config.reshard_after_forward=true \
+  trainer.policy.fsdp_config.fsdp_size=-1 \
+  trainer.fully_async.num_parallel_generation_workers=16 \
+  trainer.placement.policy_num_gpus_per_node=${NUM_TRAINING_ENGINES} \
+  trainer.placement.ref_num_gpus_per_node=${NUM_TRAINING_ENGINES} \
+  trainer.placement.policy_num_nodes=1 \
+  trainer.placement.ref_num_nodes=1 \
+  trainer.policy.sequence_parallel_size=1 \
+  generator.num_inference_engines=${NUM_INFERENCE_ENGINES} \
+  generator.inference_engine_tensor_parallel_size=1 \
+  +generator.traj_dir=${CKPT_PATH}trajectories/ \
+  +generator.engine_init_kwargs.enable_auto_tool_choice=true \
+  +generator.engine_init_kwargs.tool_call_parser=hermes \
+  +generator.engine_init_kwargs.reasoning_parser=qwen3 \
+  trainer.epochs=20 \
+  trainer.eval_batch_size=100 \
+  trainer.eval_before_train=false \
+  trainer.eval_interval=100 \
+  trainer.update_epochs_per_batch=1 \
+  trainer.train_batch_size=${BATCH_SIZE} \
+  trainer.policy_mini_batch_size=${BATCH_SIZE} \
+  trainer.micro_forward_batch_size_per_gpu=1 \
+  trainer.micro_train_batch_size_per_gpu=${MICRO_BATCH_SIZE:-1} \
+  trainer.dump_data_batch=true \
+  trainer.export_path="${CKPT_PATH}exported_model/" \
+  trainer.hf_save_interval=5 \
+  trainer.ckpt_interval=5 \
+  trainer.max_prompt_length=4096 \
+  generator.sampling_params.max_generate_length=${MAX_LENGTH} \
+  generator.sampling_params.temperature=1.0 \
+  generator.max_input_length=24000 \
+  generator.max_num_batched_tokens=48000 \
+  generator.max_turns=20 \
+  trainer.policy.optimizer_config.lr=1.0e-6 \
+  trainer.algorithm.use_kl_loss=False \
+  generator.backend=vllm \
+  generator.run_engines_locally=True \
+  generator.enable_http_endpoint=True \
+  generator.http_endpoint_host='0.0.0.0' \
+  generator.http_endpoint_port=8080 \
+  generator.weight_sync_backend=nccl \
+  generator.async_engine=true \
+  generator.batched=false \
+  generator.n_samples_per_prompt=${N_ROLLOUTS} \
+  generator.gpu_memory_utilization=0.75 \
+  generator.enforce_eager=false \
+  trainer.step_wise_training=true \
+  trainer.logger="wandb" \
+  trainer.project_name="code_search" \
+  trainer.run_name=${RUN_NAME} \
+  trainer.resume_mode=latest \
+  trainer.ckpt_path="$CKPT_PATH" \
+  trainer.max_ckpts_to_keep=3 \
+  $OTHER_OPTION
---
+++ src/distiller.py
@@ -0,0 +1,65 @@
+import torch
+import ray
+from omegaconf import DictConfig
+from skyrl_train.entrypoints.main_base import BasePPOExp
+import hydra
+from skyrl_train.trainer import RayPPOTrainer
+from skyrl_train.utils import initialize_ray
+from skyrl_train.entrypoints.main_base import config_dir, validate_cfg
+from skyrl_train.utils.ppo_utils import (
+    register_advantage_estimator,
+    register_policy_loss,
+    reduce_loss,
+)
+from skyrl_train.training_batch import TrainingInputBatch
+from skyrl_train.fully_async_trainer import FullyAsyncRayPPOTrainer
+from src.train import CodeSearchPPOExp, AsyncCodeSearchPPOExp
+
+def apply_reward_kl_penalty(data: TrainingInputBatch) -> TrainingInputBatch:
+    """Computes the KL penalty and sets the rewards to the KL penalty"""
+    loss_masks_all: torch.Tensor = data["loss_mask"]
+    teacher_action_log_probs: torch.Tensor = data["base_action_log_probs"]
+    action_log_probs: torch.Tensor = data["action_log_probs"]
+    rewards = -(action_log_probs - teacher_action_log_probs) * loss_masks_all
+    data["rewards"] = rewards
+    return data
+
+class OnPolicyDistillationTrainer(RayPPOTrainer):
+    """
+    Custom trainer for On Policy Distillation.
+
+    Overrides the apply_reward_kl_penalty method to set the rewards just to the kl penalty
+    """
+
+    def apply_reward_kl_penalty(
+        self,
+        data: TrainingInputBatch,
+    ) -> TrainingInputBatch:
+        """Computes the KL penalty and sets the rewards to the KL penalty"""
+        return apply_reward_kl_penalty(data)
+
+class FullyAsyncOnPolicyDistillationTrainer(FullyAsyncRayPPOTrainer):
+    def apply_reward_kl_penalty(
+        self,
+        data: TrainingInputBatch,
+    ) -> TrainingInputBatch:
+        return apply_reward_kl_penalty(data)
+
+
+# Using the decorator
+@register_advantage_estimator("no_op")
+def compute_no_op_advantage(token_level_rewards: torch.Tensor, **kwargs):
+    # just pass through the rewards
+    return token_level_rewards, token_level_rewards
+
+
+@register_policy_loss("importance_sampling")
+def compute_importance_sampling_policy_loss(
+    log_probs, old_log_probs, advantages, config, loss_mask=None, rollout_logprobs=None, **kwargs
+):
+    # as defined here: https://tinker-docs.thinkingmachines.ai/losses#policy-gradient-importance_sampling
+    loss = -torch.exp(log_probs - old_log_probs) * advantages
+
+    loss = reduce_loss(loss, loss_mask, "seq_mean_token_sum_norm", config.max_seq_len)
+    # return loss and a dummy clip ratio value as we aren't clipping here
+    return loss, 0.0
